9de85b6b099db94ffb9d46794be94ab76ad9c131
VitaliyRoy <vitaliybrandet@gmail.com>
2025-11-16 18:12:31 +0200

    added adaptability for phones to questions-section, thank-section

--- js/questions-section__card.js
+++ js/questions-section__card.js
@@ -1,36 +1,36 @@
-const cards = document.querySelectorAll('.questions-section__card')
+const cards = document.querySelectorAll('.questions-section__card');
 
 cards.forEach(card => {
-  const text = card.querySelector('.questions-section__card__additional-text')
+  const textContainer = card.querySelector('.questions-section__card__additional-text-container');
 
   card.addEventListener('click', () => {
-    // Закриваємо всі інші елементи
     cards.forEach(otherCard => {
       if (otherCard !== card && otherCard.classList.contains('active')) {
-        const otherText = otherCard.querySelector('.questions-section__card__additional-text')
-        otherText.style.height = otherText.scrollHeight + 'px'
+        const otherTextContainer = otherCard.querySelector('.questions-section__card__additional-text-container');
+        
+        otherTextContainer.style.height = otherTextContainer.scrollHeight + 'px';
         requestAnimationFrame(() => {
-          otherText.style.height = '0'
-        })
-        otherCard.classList.remove('active')
+          otherTextContainer.style.height = '0';
+        });
+        otherCard.classList.remove('active');
       }
-    })
+    });
 
-    // Тепер обробляємо поточний елемент
     if (card.classList.contains('active')) {
-      text.style.height = text.scrollHeight + 'px'
+      textContainer.style.height = textContainer.scrollHeight + 'px';
       requestAnimationFrame(() => {
-        text.style.height = '0'
-      })
-      card.classList.remove('active')
+        textContainer.style.height = '0';
+      });
+      card.classList.remove('active');
     } else {
-      text.style.height = text.scrollHeight + 'px'
-      card.classList.add('active')
-      text.addEventListener('transitionend', () => {
+      textContainer.style.height = textContainer.scrollHeight + 'px';
+      card.classList.add('active');
+      
+      textContainer.addEventListener('transitionend', () => {
         if (card.classList.contains('active')) {
-          text.style.height = 'auto'
+          textContainer.style.height = 'auto';
         }
-      }, { once: true })
+      }, { once: true });
     }
-  })
-})
+  });
+});
